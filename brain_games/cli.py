aebb66b2e3b8a4c90a7714626d29013c7b645917
Nikita Maslikov <nikitamaslikov@mail.ru>
2022-05-02 19:24:55 +0300

    fix cli.py and games.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,7 +4,5 @@ import prompt  # importing F to identify user
 
 
 def welcome_user():
-    """Greeting function."""
-    print('Welcome to the Brain games!')
     name = prompt.string('May I have your name? ')
     print('Hello, {}!'.format(name))
