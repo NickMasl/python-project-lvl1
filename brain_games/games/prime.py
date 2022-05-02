959597f645104a734ccca9d3d0f9ad996115c0ca
Nikita Maslikov <nikitamaslikov@mail.ru>
2022-05-02 19:03:01 +0300

    fix prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,12 +5,11 @@ GAME_RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number):
-    new_number = int(str(number)[-2:])
-    if new_number == 0:
-        return 'no'
+    if number == 0:
+        return False
     i = 2
-    while i < new_number:
-        if new_number % i == 0:
+    while i < number:
+        if number % i == 0:
             return False
         i = i + 1
     return True
