bc7a01596541b920cb0c8a5e7fc1949b812e8ab9
Nikita Maslikov <nikitamaslikov@mail.ru>
2022-05-02 18:48:41 +0300

    fix calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -6,16 +6,16 @@ GAME_RULES = 'What is the result of the expression?'
 
 def get_question_and_answer():
     operator = choice('+-*')
-    rd1 = randint(1, 10)
-    rd2 = randint(1, 10)
+    random_digit1 = randint(1, 10)
+    random_digit2 = randint(1, 10)
 
     def solve():
         if operator == '+':
-            return rd1 + rd2
+            return random_digit1 + random_digit2
         elif operator == '-':
-            return rd1 - rd2
+            return random_digit1 - random_digit2
         elif operator == '*':
-            return rd1 * rd2
+            return random_digit1 * random_digit2
     correct_answer = str(solve())
-    question = '{} {} {}'.format(rd1, operator, rd2)
+    question = '{} {} {}'.format(random_digit1, operator, random_digit2)
     return question, correct_answer
