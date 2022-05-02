1499c77d8b689a1bd39abe4d735eaefb9163c14e
Nikita Maslikov <nikitamaslikov@mail.ru>
2022-05-02 20:30:30 +0300

    fix calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -8,14 +8,13 @@ def get_question_and_answer():
     operator = choice('+-*')
     random_digit1 = randint(1, 10)
     random_digit2 = randint(1, 10)
-
-    def solve():
-        if operator == '+':
-            return random_digit1 + random_digit2
-        elif operator == '-':
-            return random_digit1 - random_digit2
-        elif operator == '*':
-            return random_digit1 * random_digit2
-    correct_answer = str(solve())
     question = '{} {} {}'.format(random_digit1, operator, random_digit2)
-    return question, correct_answer
+    if operator == '+':
+        correct_answer = str(random_digit1 + random_digit2)
+        return question, correct_answer
+    elif operator == '-':
+        correct_answer = str(random_digit1 - random_digit2)
+        return question, correct_answer
+    elif operator == '*':
+        correct_answer = str(random_digit1 * random_digit2)
+        return question, correct_answer
