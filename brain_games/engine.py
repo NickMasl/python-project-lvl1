6eb37c067eeb56650260e1788da27c5e5ba393b5
Nikita Maslikov <nikitamaslikov@mail.ru>
2022-05-04 01:33:35 +0300

    fix engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -9,13 +9,16 @@ def run_game(game):
     name = prompt.string('May I have your name? ')
     print('Hello, {}!'.format(name))
     print(game.GAME_RULES)
-    for currect_round in range(NUMBER_OF_ROUNDS):
+    for _ in range(NUMBER_OF_ROUNDS):
         question, correct_answer = game.get_question_and_answer()
         print('Question: {}'.format(question))
         answer = prompt.string('Your answer: ')
         if answer == correct_answer:
             print('Correct!')
         else:
-            print("'{}' is wrong answer ;( Correct answer was '{}'.\nLet's try again, {}!".format(answer, correct_answer, name))  # noqa: E501
+            print(
+                f"'{answer}' is wrong answer ;("
+                f" Correct answer was '{correct_answer}'."
+                f"\nLet's try again, {name}!")
             return
     print('Congratulations, {}!'.format(name))
